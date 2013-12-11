1cc299ef04fa024dc1899e9a1350356438cca648
jizilong <jizilong1988@163.com>
2013-12-11 21:41:43 +0800

    init and create product

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DepotRails4::Application.config.secret_key_base = 'c65205162f4193846974303c05358d7b938c03a387ee49ac55419b8027a8a3c5b38aab697a08cdbe78fa28622a67e690bcc4b54e7d8f5d82071f2979dd44ea3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DepotRails4::Application.config.session_store :cookie_store, key: '_depot-rails4_session'
